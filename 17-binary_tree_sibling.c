a5c7b7a5dc351aac90637e14aeb33cb70826de63
salahbesbes <salah_besbes@ymail.com>
2021-03-11 19:23:38 +0100

    return siblig

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,5 +11,8 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	return (node->parent->right);
+	if (node->parent->right == node)
+		return (node->parent->left);
+	else
+		return (node->parent->right);
 }
