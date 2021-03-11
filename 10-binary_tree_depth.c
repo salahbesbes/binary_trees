0fd811405050db78007b93a3edd5a5a3c9f596b1
salahbesbes <salah_besbes@ymail.com>
2021-03-11 18:23:23 +0100

    need betty check

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -7,6 +7,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 
 	if (tree == NULL)
 		return (0);
+
 	while (tree->parent)
 	{
 		depth ++;
--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -6,7 +6,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->right || tree->left)
+	if (tree->left || tree->right)
 		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->left));
 	else
 		return (0);
